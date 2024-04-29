6a6c6f11b01f5fc7d95fd80ce37e1ec9b8167d01
mathpro28 <matemerc@gmail.com>
2024-04-29 18:20:11 -0400

    Initial Commit

---
+++ MoodScape/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodScape
+//
+//  Created by Mateo Mercado Maguiña on 29/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodScape/MoodScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodScapeApp.swift
+//  MoodScape
+//
+//  Created by Mateo Mercado Maguiña on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
