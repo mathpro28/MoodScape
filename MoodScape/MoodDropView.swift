31e25c11abc7ffb3de9032d04a5f25335926d680
mathpro28 <matemerc@gmail.com>
2024-07-01 19:27:43 -0400

    Change from top to bottom to leading to trailing

--- MoodScape/MoodDropView.swift
+++ MoodScape/MoodDropView.swift
@@ -21,54 +21,33 @@ struct MoodDropView: View {
         "Confused": "😕"
     ]
     @State private var randomMoods: [String] = []
-    @State private var offsets: [CGFloat] = []
-    
+    @State private var offsets: [CGSize] = []
+
     var body: some View {
         ZStack {
             ForEach(0..<randomMoods.count, id: \.self) { index in
                 Text(randomMoods[index])
-                    .font(.footnote)
-                    .bold()
-                    .offset(y: offsets[index])
+                    .font(.largeTitle)
+                    .offset(offsets[index])
                     .animation(Animation.linear(duration: 5).repeatForever(autoreverses: false), value: offsets[index])
             }
         }
         .onAppear(perform: startDroppingMoods)
     }
-    
+
     func startDroppingMoods() {
         randomMoods = moods.keys.shuffled().prefix(5).map { $0 }
-        offsets = Array(repeating: -100, count: randomMoods.count)
-        
+        offsets = Array(repeating: CGSize(width: -UIScreen.main.bounds.width, height: 0), count: randomMoods.count)
+
         for i in 0..<randomMoods.count {
+            let yPosition = CGFloat(i) * 50 // Adjust vertical spacing
+            offsets[i] = CGSize(width: -UIScreen.main.bounds.width, height: yPosition)
             withAnimation(Animation.linear(duration: 5).repeatForever(autoreverses: false).delay(Double(i) * 0.5)) {
-                offsets[i] = UIScreen.main.bounds.height + 100
+                offsets[i] = CGSize(width: UIScreen.main.bounds.width + 100, height: yPosition)
             }
         }
     }
 }
-
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Text("How are you feeling today?")
-//                .bold()
-//                .foregroundColor(.white)
-//                .font(.largeTitle)
-//                .frame(alignment: .trailing)
-//            MoodDropView()
-//                .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        }
-//        .background(Color.black.edgesIgnoringSafeArea(.all))
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
-
 
 #Preview {
     MoodDropView()
