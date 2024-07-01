90f81160c36ff385de5a59434cc4d6fc232e51f9
mathpro28 <matemerc@gmail.com>
2024-07-01 19:23:53 -0400

    Add a dictionary view with different moods

---
+++ MoodScape/MoodDropView.swift
@@ -0,0 +1,18 @@
+//
+//  MoodDropView.swift
+//  MoodScape
+//
+//  Created by Mateo Mercado Maguiña on 1/7/24.
+//
+
+import SwiftUI
+
+struct MoodDropView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodDropView()
+}
