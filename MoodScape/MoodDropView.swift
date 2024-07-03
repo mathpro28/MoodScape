a97553e03a87b19ea50fe17978dfda424096bd7d
mathpro28 <matemerc@gmail.com>
2024-07-02 20:30:14 -0400

    Add a speed for the animation

--- MoodScape/MoodDropView.swift
+++ MoodScape/MoodDropView.swift
@@ -22,29 +22,29 @@ struct MoodDropView: View {
     ]
     @State private var randomMoods: [String] = []
     @State private var offsets: [CGSize] = []
+    @State private var animationDuration: Double = 7.0
 
     var body: some View {
         ZStack {
             ForEach(0..<randomMoods.count, id: \.self) { index in
                 Text(randomMoods[index])
-                    .font(.footnote)
+                    .font(.custom("Courier New", size: 15))
                     .bold()
-                    .foregroundStyle(.white)
+                    .foregroundColor(.white)
                     .offset(offsets[index])
-                    .animation(Animation.linear(duration: 5).repeatForever(autoreverses: true), value: offsets[index])
             }
         }
         .onAppear(perform: startDroppingMoods)
     }
 
     func startDroppingMoods() {
-        randomMoods = moods.keys.shuffled().prefix(5).map { $0 }
+        randomMoods = moods.keys.shuffled().prefix(6).map { $0 }
         offsets = Array(repeating: CGSize(width: -UIScreen.main.bounds.width, height: 0), count: randomMoods.count)
 
         for i in 0..<randomMoods.count {
-            let yPosition = CGFloat(i) * 50 // Adjust vertical spacing
+            let yPosition = CGFloat(i) * 20 // Adjust vertical spacing
             offsets[i] = CGSize(width: -UIScreen.main.bounds.width, height: yPosition)
-            withAnimation(Animation.linear(duration: 5).repeatForever(autoreverses: false).delay(Double(i) * 0.5)) {
+            withAnimation(Animation.linear(duration: animationDuration).repeatForever(autoreverses: true).delay(Double(i) * 0.2)) {
                 offsets[i] = CGSize(width: UIScreen.main.bounds.width + 100, height: yPosition)
             }
         }
