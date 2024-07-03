e47de8b1f40af18feac0ee86d6bcbdbe2efc2e73
mathpro28 <matemerc@gmail.com>
2024-07-02 22:46:43 -0400

    Fix words animation but will comment it from ContentView

--- MoodScape/MoodDropView.swift
+++ MoodScape/MoodDropView.swift
@@ -22,33 +22,44 @@ struct MoodDropView: View {
     ]
     @State private var randomMoods: [String] = []
     @State private var offsets: [CGSize] = []
-    @State private var animationDuration: Double = 7.0
+    @State private var animationDuration: Double = 1.0
 
     var body: some View {
-        ZStack {
-            ForEach(0..<randomMoods.count, id: \.self) { index in
-                Text(randomMoods[index])
-                    .font(.custom("Courier New", size: 15))
-                    .bold()
-                    .foregroundColor(.white)
-                    .offset(offsets[index])
+        VStack {
+            Spacer()
+            ZStack {
+                ForEach(0..<randomMoods.count, id: \.self) { index in
+                    Text(randomMoods[index])
+//                        .font(.custom("Courier New", size: 20))
+                        .bold()
+                        .shadow(radius: 0.1)
+                        .foregroundColor(.white)
+                        .offset(offsets[index])
+                        .onAppear {
+                            animateWord(at: index)
+                        }
+                }
             }
-        }
-        .onAppear(perform: startDroppingMoods)
-    }
-
-    func startDroppingMoods() {
-        randomMoods = moods.keys.shuffled().prefix(6).map { $0 }
-        offsets = Array(repeating: CGSize(width: -UIScreen.main.bounds.width, height: 0), count: randomMoods.count)
-
-        for i in 0..<randomMoods.count {
-            let yPosition = CGFloat(i) * 20 // Adjust vertical spacing
-            offsets[i] = CGSize(width: -UIScreen.main.bounds.width, height: yPosition)
-            withAnimation(Animation.linear(duration: animationDuration).repeatForever(autoreverses: true).delay(Double(i) * 0.2)) {
-                offsets[i] = CGSize(width: UIScreen.main.bounds.width + 100, height: yPosition)
-            }
+            .onAppear(perform: startDroppingMoods)
+            Spacer()
         }
     }
+
+        func startDroppingMoods() {
+            randomMoods = moods.keys.shuffled().prefix(6).map { $0 }
+            offsets = Array(repeating: CGSize(width: -UIScreen.main.bounds.width, height: 0), count: randomMoods.count)
+            
+            for i in 0..<randomMoods.count {
+                let yPosition = CGFloat(i) * 20 // Adjust vertical spacing
+                offsets[i] = CGSize(width: -UIScreen.main.bounds.width / 2 + 150, height: yPosition)
+            }
+        }
+
+        func animateWord(at index: Int) {
+            withAnimation(Animation.linear(duration: animationDuration).repeatForever(autoreverses: true)) {
+                offsets[index] = CGSize(width: UIScreen.main.bounds.width / 2 - 150, height: offsets[index].height)
+            }
+        }
 }
 
 #Preview {
