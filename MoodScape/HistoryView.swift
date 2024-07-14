609db13cb72d28524e37f0b46e5ac2d3d41215c9
mathpro28 <matemerc@gmail.com>
2024-07-13 22:50:26 -0400

    Add a dummy view in order to fetch all of the data and verify that the implementation is working properly

---
+++ MoodScape/HistoryView.swift
@@ -0,0 +1,51 @@
+//
+//  HistoryView.swift
+//  MoodScape
+//
+//  Created by Mateo Mercado Maguiña on 29/4/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HistoryView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Query(sort: \Register.date, order: .reverse) private var entries: [Register]
+    
+    var body: some View {
+        VStack {
+            Text("History")
+                .bold()
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .padding()
+            
+            List(entries) { entry in
+                HStack {
+                    Text("Rating: \(entry.value)")
+                    Spacer()
+                    Text(entry.date, style: .date)
+                }
+            }
+            .listStyle(PlainListStyle())
+        }
+        .padding()
+        .navigationTitle("Summary")
+    }
+}
+
+#Preview {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Register.self, configurations: config)
+
+    // Insert some dummy data for preview purposes
+    let entry1 = Register(value: 4, date: .now)
+    let entry2 = Register(value: 3, date: .now.addingTimeInterval(-86400)) // 1 day ago
+
+    container.mainContext.insert(entry1)
+    container.mainContext.insert(entry2)
+
+    return HistoryView()
+        .modelContainer(container)
+}
