c697c85d3f0c3c630abe07bf4ba4d3f491fbd8d4
mathpro28 <user2@MacBook-Pro-de-Mateo.local>
2024-07-31 20:31:20 -0400

    Add the support for the charts picker between daily, weekly and monthly

--- MoodScape/ChartsView.swift
+++ MoodScape/ChartsView.swift
@@ -9,29 +9,47 @@ import SwiftUI
 import SwiftUICharts
 
 struct ChartsView: View {
-    @Environment(\.modelContext) private var context
-    
+    @State private var defaultSelectedSummary = "Weekly"
+    private let summaryOptions = ["Daily", "Weekly", "Monthly"]
+
     var body: some View {
-        
         VStack {
-            Text("Summary")
-                .bold()
-                .foregroundColor(.black)
-                .font(.largeTitle)
-                .padding()
+//            Text("Summary")
+//                .bold()
+//                .foregroundColor(.black)
+//                .font(.largeTitle)
+//                .padding()
             
-            // Line chart with data points
-            LineView(data: [5, 5, 3, 2, 1, 3, 3], title: "Weekly Summary", legend: "Mood")
-                .frame(height: 300)
-                .padding()
+            Picker("Summary", selection: $defaultSelectedSummary) {
+                ForEach(summaryOptions, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            if defaultSelectedSummary == "Daily" {
+                // Insert daily summary content here
+                LineView(data: [1, 2, 3, 4, 5, 6, 7], title: "Daily Summary", legend: "Mood")
+                    .frame(height: 300)
+                    .padding()
+            } else if defaultSelectedSummary == "Weekly" {
+                // Weekly summary content
+                LineView(data: [5, 5, 3, 2, 1, 3, 3], title: "Weekly Summary", legend: "Mood")
+                    .frame(height: 300)
+                    .padding()
+            } else if defaultSelectedSummary == "Monthly" {
+                // Insert monthly summary content here
+                LineView(data: [1, 3, 2, 4, 3, 5, 6, 7, 4, 3, 2, 1], title: "Monthly Summary", legend: "Mood")
+                    .frame(height: 300)
+                    .padding()
+            }
         }
     }
 }
 
-struct LineChartExample_Previews: PreviewProvider {
+struct ChartsView_Previews: PreviewProvider {
     static var previews: some View {
         ChartsView()
     }
 }
-
-
